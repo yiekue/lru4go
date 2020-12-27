f271e4f006451399eb3c0e47a238cfa5505df631
coderic <1015707909@qq.com>
2020-12-27 13:05:52 +0800

    单元测试更新

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
 # lru4go
 使用golang实现的LRU缓存.
 
-+ 线程安全
-+ 支持设置过期时间
-
 example:
 ```go
 
 cache := lru4go.New(2000)
 cache.Set("key","value")
-cache.Set("ttl","20 second", 20)
+cache.Set("ttl","20 second")
 if v, ok:=cache.Get(key); !ok {
     fmt.Println("get failed:", ok.Error())
 }
--- lru4go_test.go
+++ lru4go_test.go
@@ -1,6 +1,7 @@
 package lru4go
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -43,22 +44,6 @@ func TestLrucache_Keys(t *testing.T) {
 	}
 }
 
-func TestLrucache_Keys_TTL(t *testing.T) {
-	cache, _ := New(50)
-	cache.Set("t1", 1)
-	cache.Set("t2", 2)
-
-	keys := cache.Keys()
-	if nil == keys {
-		t.Fatal("keys is nil")
-	}
-	for _, v := range keys {
-		if "t1" != v && "t2" != v {
-			t.Error("keys wrong,key=", v)
-		}
-	}
-}
-
 func TestLrucache_Delete(t *testing.T) {
 	cache, _ := New(50)
 	cache.Set("t1", 1)
@@ -69,5 +54,18 @@ func TestLrucache_Delete(t *testing.T) {
 	v, _ := cache.Get("t1")
 	if v != nil {
 		t.Error("delete failed!")
+	}
+}
+
+func TestLrucache_expireOldest(t *testing.T) {
+	max := 10
+	cache, _ := New(max)
+	for i := 0; i <= max+1; i++ {
+		cache.Set(fmt.Sprintf("%d", i), i)
+	}
+
+	v, err := cache.Get(fmt.Sprintf("%d", 0))
+	if err == nil {
+		t.Error("expire oldest failed:", v)
 	}
 }
